5af1f9b157d8b7653f3ddb84f72a88457a8c982e
cenfraGit <161994865+cenfraGit@users.noreply.github.com>
2025-05-04 09:19:38 -0600

    change interface layout

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import wx
 import cv2
 from utils import dip
 from panel_camera import PanelCamera
+from panel_view import PanelView
 
 import ctypes
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
@@ -19,30 +20,30 @@ class FrameMain(wx.Frame):
         # ------------- frame setup ------------- #
         
         self.SetTitle("Rocky")
-        self.SetClientSize(dip(300, 300))
+        self.SetClientSize(dip(800, 500))
 
         # all subpanels will be children of the main panel
         self.panel_main = wx.Panel(self)
         self.sizer = wx.GridBagSizer()
         self.panel_main.SetSizer(self.sizer)
 
+        # -------------- panel view -------------- #
+
+        self.panel_view = PanelView(self.panel_main)
+        self.panel_view.SetBackgroundColour(wx.GREEN)
+        self.sizer.Add(self.panel_view, pos=(0, 0), flag=wx.EXPAND)
+
         # -------------- panel cam -------------- #
         
         self.capture = cv2.VideoCapture(0)
         self.panel = PanelCamera(self.panel_main, self.capture)
-        self.sizer.Add(self.panel, pos=(0, 0), flag=wx.EXPAND)
+        self.sizer.Add(self.panel, pos=(0, 1), flag=wx.EXPAND)
 
-        # -------------- panel view -------------- #
+        # ------------- bottom panel ------------- #
 
-        self.panel_view = wx.Panel(self.panel_main)
-        self.panel_view.SetBackgroundColour(wx.GREEN)
-        self.sizer.Add(self.panel_view, pos=(0, 1), flag=wx.EXPAND)
-
-        # ---------- panel instructions ---------- #
-
-        self.panel_instructions = wx.Panel(self.panel_main)
-        self.panel_instructions.SetBackgroundColour(wx.RED)
-        self.sizer.Add(self.panel_instructions, pos=(1, 0), span=(1, 2), flag=wx.EXPAND)
+        self.panel_bottom = wx.Panel(self.panel_main)
+        self.panel_bottom.SetBackgroundColour(wx.RED)
+        self.sizer.Add(self.panel_bottom, pos=(1, 0), span=(1, 2), flag=wx.EXPAND)
 
         # ----------- sizer growables ----------- #
         
@@ -51,6 +52,48 @@ class FrameMain(wx.Frame):
         self.sizer.AddGrowableRow(0, 1)
         self.sizer.AddGrowableRow(1, 1)
         self.sizer.Layout()
+
+        # ------------------------------------------------------------
+        # menubar
+        # ------------------------------------------------------------
+
+        menubar = wx.MenuBar()
+        menu_configuration = wx.Menu()
+        item_connect = wx.MenuItem(menu_configuration, -1, "&Connect...\tCtrl+C", "Connect to Rocky.")
+        # submenu for control mode
+        submenu_control = wx.Menu()
+        radio_control_manual = wx.MenuItem(submenu_control, wx.ID_ANY, "Manual", kind=wx.ITEM_RADIO)
+        radio_control_voice = wx.MenuItem(submenu_control, wx.ID_ANY, "Voice", kind=wx.ITEM_RADIO)
+        submenu_control.Append(radio_control_manual)
+        submenu_control.Append(radio_control_voice)
+        item_exit = wx.MenuItem(menu_configuration, wx.ID_EXIT, "&Exit...\tCtrl+C", "Exit the application.")
+        menu_configuration.Append(item_connect)
+        menu_configuration.AppendSubMenu(submenu_control, "Control", "Change control mode.")
+        menu_configuration.AppendSeparator()
+        menu_configuration.Append(item_exit)
+
+        menu_help = wx.Menu()
+        item_log = wx.MenuItem(menu_help, -1, "&Log...\tCtrl+T", "See log.")
+        menu_help.Append(item_log)
+
+        menubar.Append(menu_configuration, "Configuration")
+        menubar.Append(menu_help, "Help")
+        
+        self.SetMenuBar(menubar)
+
+        self.Bind(wx.EVT_MENU, self.OnExit, item_exit)
+
+        # -------------- statusbar -------------- #
+
+        self.statusbar = self.CreateStatusBar()
+        self.SetStatus("Offline")
+
+    def SetStatus(self, text):
+        self.statusbar.SetStatusText(f"Status: {text}")
+
+    def OnExit(self, event):
+        self.Close()
+
 
 if __name__ == "__main__":
     app = wx.App()
--- panel_view.py
+++ panel_view.py
@@ -3,9 +3,54 @@
 import wx
 import wx.glcanvas as glcanvas
 import numpy as np
-from OpenGL.GL import *
-import OpenGL.GL.shaders
+from OpenGL import GL
+from OpenGL.GL import shaders
 
 class PanelView(glcanvas.GLCanvas):
     def __init__(self, parent):
+
+        dispAttrs = glcanvas.GLAttributes()
+        dispAttrs.PlatformDefaults().Depth(16).DoubleBuffer().SampleBuffers(4).Samplers(4).EndList()
+
+        super().__init__(parent, dispAttrs, size=wx.Size(300, 300))
+
+        self.context = None
+        self.init = False
+
+        self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+
+    def InitGL(self):
+        if self.context is None:
+            self.context = glcanvas.GLContext(self)
+        self.SetCurrent(self.context)
+        if not self.init:
+            GL.glClearColor(0.0, 0.0, 0.0, 1.0)
+            self.init = True
+
+    def OnEraseBackground(self, event):
         pass
+
+    def OnSize(self, event):
+        size = self.GetClientSize()
+        if self.context:
+            self.SetCurrent(self.context)
+            GL.glViewport(0, 0, size.width, size.height)
+        event.Skip()
+
+    def OnPaint(self, event):
+        self.InitGL()
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
+        GL.glBegin(GL.GL_TRIANGLES)
+        GL.glColor3f(1.0, 0.0, 0.0)
+        GL.glVertex3f(0.0, 0.5, 0.0)
+        GL.glColor3f(0.0, 1.0, 0.0)
+        GL.glVertex3f(-0.5, -0.5, 0.0)
+        GL.glColor3f(0.0, 0.0, 1.0)
+        GL.glVertex3f(0.5, -0.5, 0.0)
+        GL.glEnd()
+
+        self.SwapBuffers()
+        event.Skip()
+        
